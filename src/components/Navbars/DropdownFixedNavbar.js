6c4e2c23dd27fb7a83b164d71638810df8e40c46
Golnaz8 <golnaz.brj@gmail.com>
2024-02-15 11:47:50 -0500

    added link of my resume

--- src/components/Navbars/DropdownFixedNavbar.js
+++ src/components/Navbars/DropdownFixedNavbar.js
@@ -91,7 +91,7 @@ function IconFixedNavbar() {
                 <Button
                   className="nav-link btn-default"
                   color="neutral"
-                  href="#"
+                  href="https://docs.google.com/document/d/1vM2DqdbGt_vgWES2FEe3WXpgcphhYB-7/edit?usp=drive_link&ouid=114168789201802893623&rtpof=true&sd=true"
                   target="_blank"
                 >
                   <p>My Resume</p>
